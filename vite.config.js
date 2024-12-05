1a29248a81d5c7348d3c7ad63c8923ca7a40291a
yoginandal <yogidesign1996@gmail.com>
2024-12-05 14:45:18 +0530

    trying again

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // This is important for routing
+  base: "/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
-    historyApiFallback: true, // Enable client-side routing
+    historyApiFallback: true,
   },
   build: {
     rollupOptions: {
